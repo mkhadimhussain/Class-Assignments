43740d0e73f4a3ff59e76d1cd40ae35bd200a3bd
mkhadimhussain <163546834+mkhadimhussain@users.noreply.github.com>
2024-03-26 01:43:17 +0500

    class assignments

---
+++ Assignment05/main.js
@@ -0,0 +1,5 @@
+//This is the code of "main.ts file" in which we importing the function
+import myMarksheet from "./marksheet.js";
+//Arguments to the function
+const result = myMarksheet(98, 92, 88, 79, 95);
+console.log(`My Result is : ${result}%`);
